058d96e0dd9064e47d41cd1e8f32db1ae482fd6b
Nono <nono@bxb.tw>
2018-11-27 00:07:37 +0800

    finish upload & random

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,77 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button
+      @click="doUpload"
+    >
+      上傳
+    </button>
+    <button
+      @click="randomPig"
+    >
+      抽獎
+    </button>
+    <div v-show="isEnd">
+      恭喜!!得獎的是!!
+    </div>
+    <input
+      type="file"
+      ref="file"
+      @change="handleUpload"
+      multiple
+    >
+    <PhotoFrame
+      msg="Halo nono 123 5566sss"
+      :src="src"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import PhotoFrame from './components/PhotoFrame.vue'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    PhotoFrame
+  },
+  data () {
+    return {
+      baseUrl: process.env.BASE_URL,
+      src: '',
+      images: [],
+      count: 0,
+      isEnd: ''
+    }
+  },
+  methods: {
+    doUpload () {
+      this.$refs.file.click()
+    },
+    handleUpload (event) {
+      const reader = new FileReader()
+      const files = Array.from(event.target.files)
+      files.forEach(file => {
+        let reader = new FileReader()
+        reader.addEventListener('load', () => {
+          this.images.push(reader.result)
+          this.src = reader.result
+        })
+        reader.readAsDataURL(file)
+      })
+    },
+    randomPig (timeout) {
+      this.isEnd = false
+      this.src = this.images[this.getRandom(0, this.images.length)]
+      this.count++
+      if (this.count < 100) {
+        setTimeout(() => this.randomPig(this.count * 1.5), timeout)
+      } else {
+        this.count = 0
+        this.isEnd = true
+      }
+    },
+    getRandom (min, max) {
+      return Math.floor(Math.random() * (max - min)) + min
+    }
   }
 }
 </script>
@@ -24,5 +84,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+input[type="file"] {
+  display: none
 }
 </style>
